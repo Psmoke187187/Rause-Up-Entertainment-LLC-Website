a61fbfb78bd5389d83aa29ad3cc990a6f35acbd3
Psmoke187187 <gsantiago0829@gmail.com>
2026-07-24 15:31:16 -0400

    Add four new beats to the catalog

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -38,5 +38,45 @@ window.RAISEUP_BEATS = [
     preview: "assets/audio/previews/motovation-preview.mp3",
     price: "$100",
     checkout: "mailto:raiseupent.ru@gmail.com?subject=Motovation%20Beat%20License%20-%20%24100&body=Hi%20Raise%20Up%20Entertainment%2C%0A%0AI%20want%20to%20purchase%20the%20Motovation%20beat%20license%20for%20%24100.%0A%0AMy%20name%3A%20%0AMy%20artist%20name%3A%20"
+  },
+  {
+    id: "bad-bitch",
+    title: "Bad B*tch",
+    genre: "Trap",
+    mood: "Bold, glamorous, high-status energy",
+    cover: "assets/img/covers/bad-bitch-cover.webp",
+    preview: "assets/audio/previews/bad-bitch-preview.mp3",
+    price: "$100",
+    checkout: "mailto:raiseupent.ru@gmail.com?subject=Bad%20B*tch%20Beat%20License%20-%20%24100&body=Hi%20Raise%20Up%20Entertainment%2C%0A%0AI%20want%20to%20purchase%20the%20Bad%20B*tch%20beat%20license%20for%20%24100.%0A%0AMy%20name%3A%20%0AMy%20artist%20name%3A%20"
+  },
+  {
+    id: "country-hore-beat",
+    title: "Country Hore Beat",
+    genre: "Country Trap",
+    mood: "Rebellious western crossover energy",
+    cover: "assets/img/covers/country-hore-beat-cover.webp",
+    preview: "assets/audio/previews/country-hore-beat-preview.mp3",
+    price: "$100",
+    checkout: "mailto:raiseupent.ru@gmail.com?subject=Country%20Hore%20Beat%20Beat%20License%20-%20%24100&body=Hi%20Raise%20Up%20Entertainment%2C%0A%0AI%20want%20to%20purchase%20the%20Country%20Hore%20Beat%20beat%20license%20for%20%24100.%0A%0AMy%20name%3A%20%0AMy%20artist%20name%3A%20"
+  },
+  {
+    id: "fuck-that-bitch",
+    title: "F*ck That B*tch",
+    genre: "Trap",
+    mood: "Defiant, cold breakup energy",
+    cover: "assets/img/covers/fuck-that-bitch-cover.webp",
+    preview: "assets/audio/previews/fuck-that-bitch-preview.mp3",
+    price: "$100",
+    checkout: "mailto:raiseupent.ru@gmail.com?subject=F*ck%20That%20B*tch%20Beat%20License%20-%20%24100&body=Hi%20Raise%20Up%20Entertainment%2C%0A%0AI%20want%20to%20purchase%20the%20F*ck%20That%20B*tch%20beat%20license%20for%20%24100.%0A%0AMy%20name%3A%20%0AMy%20artist%20name%3A%20"
+  },
+  {
+    id: "nite-light",
+    title: "Nite Light",
+    genre: "R&B",
+    mood: "Dreamy, nocturnal melodic atmosphere",
+    cover: "assets/img/covers/nite-light-cover.webp",
+    preview: "assets/audio/previews/nite-light-preview.mp3",
+    price: "$100",
+    checkout: "mailto:raiseupent.ru@gmail.com?subject=Nite%20Light%20Beat%20License%20-%20%24100&body=Hi%20Raise%20Up%20Entertainment%2C%0A%0AI%20want%20to%20purchase%20the%20Nite%20Light%20beat%20license%20for%20%24100.%0A%0AMy%20name%3A%20%0AMy%20artist%20name%3A%20"
   }
 ];
